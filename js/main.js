6eb042937b66ca0d32fae576265d2026d872ec97
Ethan Chiou <ethanchiou@berkeley.edu>
2017-09-23 20:40:02 -0700

    clean up javascript

--- js/main.js
+++ js/main.js
@@ -1,79 +1,35 @@
 window.onload = function(){
-  if ($(this).scrollTop() > 110) {
-    $('.scrollToTop').fadeIn(500);
+  // Check to see if the window is top on load
+  // If not, then display button
+  if ($(this).scrollTop() > 100) {
+    $(".scrollToTop").css("opacity", "1");
   }
-  //Check to see if the window is top if not then display button
+  // Check to see if the window is top on scroll
+  // If not, then display button
   $(window).scroll(function(){
-    if ($(this).scrollTop() > 110) {
-      $('.scrollToTop').fadeIn(500);
+    if ($(this).scrollTop() > 100) {
+      $(".scrollToTop").css("opacity", "1");
     } else {
-      $('.scrollToTop').fadeOut(500);
+      $(".scrollToTop").css("opacity", "0");
     }
   });
 
-  //Click event to scroll to top
+  // Click event to scroll to top
   $('.scrollToTop').click(function(){
     $('html, body').animate({scrollTop : 0},1000);
     return false;
   });
 
-  $( "#index-photos" )
-  .mouseenter(function() {
+  // Index page image fading
+  $( "#index-photos" ).mouseenter(function() {
     $( "#index-pic" ).css("background-image", "url\(\"/images/play.jpg\"\)");
-  })
-  .mouseleave(function() {
-    // $( "#index-pic" ).css("background-image", "none");
   });
 
-  $( "#index-mindtribe" )
-  .mouseenter(function() {
+  $( "#index-mindtribe" ).mouseenter(function() {
     $( "#index-pic" ).css("background-image", "url\(\"/images/mindtribe.jpg\"\)");
-  })
-  .mouseleave(function() {
-    // $( "#index-pic" ).css("background-image", "none");
   });
 
-  $( "#index-hci" )
-  .mouseenter(function() {
+  $( "#index-hci" ).mouseenter(function() {
     $( "#index-pic" ).css("background-image", "url\(\"/images/hci.jpg\"\)");
-  })
-  .mouseleave(function() {
-    // $( "#index-pic" ).css("background-image", "none");
-  });
-
-  $(function() {
-      $("#contact-form").submit(function(e) {
-        e.preventDefault();
-        submitButton = document.getElementById("contact-submit");
-        $.ajax({
-          url: "//formspree.io/ethanchiou@berkeley.edu",
-          method: "POST",
-          data: $(this).serialize(),
-          dataType: "json",
-          beforeSend: function() {
-            submitButton.innerHTML = '<span class=\"fa fa-spin fa-circle-o-notch\"></span>';
-          },
-          success: function(data){
-            // Success message
-            submitButton.innerHTML = '';
-            $('#contact-overlay').fadeIn();
-
-            setTimeout(function(){
-              //clear all fields
-              $('#contact-form').trigger("reset");
-            }, 500);
-          },
-          error: function(){
-            // Success message
-            submitButton.innerHTML = 'Error. Please try again later.';
-
-            setTimeout(function(){
-              submitButton.innerHTML = 'Submit';
-            }, 2000);
-            //clear all fields
-            $('#contact-form').trigger("reset");
-          }
-        });
-      });
   });
 }
