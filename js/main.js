b8987d2708859ab0be200ed5222aedca21b5598b
Ethan C <ethan@mindtribe.com>
2019-01-20 23:35:28 -0800

    Fix scroll to top

--- js/main.js
+++ js/main.js
@@ -2,20 +2,23 @@ window.onload = function(){
   // Check to see if the window is top on load
   // If not, then display button
   if ($(this).scrollTop() > 100) {
-    $(".scrollToTop").css("opacity", "1");
+    $(".scroll-to-top").css("opacity", "1");
+    $(".scroll-to-top").css("pointer-events", "auto");
   }
   // Check to see if the window is top on scroll
   // If not, then display button
   $(window).scroll(function(){
     if ($(this).scrollTop() > 100) {
-      $(".scrollToTop").css("opacity", "1");
+      $(".scroll-to-top").css("opacity", "1");
+      $(".scroll-to-top").css("pointer-events", "auto");
     } else {
-      $(".scrollToTop").css("opacity", "0");
+      $(".scroll-to-top").css("opacity", "0");
+      $(".scroll-to-top").css("pointer-events", "none");
     }
   });
 
   // Click event to scroll to top
-  $('.scrollToTop').click(function(){
+  $('.scroll-to-top').click(function(){
     $('html, body').animate({scrollTop : 0},1000);
     return false;
   });
