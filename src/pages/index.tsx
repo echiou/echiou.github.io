ca82f24241e27a77be1bbf518e6e361023930ca6
Ethan Chiou <ethan@span.io>
2022-05-27 22:58:34 -0700

    New website

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+// import 'cirrus-ui';
+import * as React from "react";
+// import '@fontsource/cutive-mono';
+
+// const mono = {
+//   fontFamily: 'Cutive Mono',
+// };
+
+const IndexPage: React.FC = () => {
+  return <main>hi</main>;
+};
+
+export default IndexPage;
